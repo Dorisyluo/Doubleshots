c1284a6b952da2fa6424f35e80d9733494128ce1
YashVSolanki <38870520+YashVSolanki@users.noreply.github.com>
2020-01-16 00:17:04 -0800

    Got basic waypoint movement working

---
+++ Double Shots/Assets/Scripts/Patron_Scripts/Move_to.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_to : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] chairs;
+    public float speed;
+    private int index;
+    private GameObject targetChair;
+    // Update is called once per frame
+    void Start()
+    {
+        chairs = GameObject.FindGameObjectsWithTag("Seat");
+        index = Random.Range(0, chairs.Length);
+        targetChair = chairs[index];
+    }
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(targetChair.transform.position.x,transform.position.y, targetChair.transform.position.z), step);
+       
+    }
+
+}
