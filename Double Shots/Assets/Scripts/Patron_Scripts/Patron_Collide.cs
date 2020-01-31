27d8fbfe591fbb9f97150fbb2f676b425e7582ea
YashVSolanki <38870520+YashVSolanki@users.noreply.github.com>
2020-01-30 23:09:42 -0800

    Added Projectile Tag, Collider SHOULD print to console

--- Double Shots/Assets/Scripts/Patron_Scripts/Patron_Collide.cs
+++ Double Shots/Assets/Scripts/Patron_Scripts/Patron_Collide.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class Patron_Collide : MonoBehaviour
 {
     private GameObject collideObject;
+    private int typeCompare;
     // Update is called once per frame
-    void Update()
+    private void Start()
     {
- 
+        switch (GetComponent<Patron_Data>().wantedDrink)
+        {
+            case "red":
+                typeCompare = 1;
+                break;
+            case "green":
+                typeCompare = 2;
+                break;
+            case "blue":
+                typeCompare = 3;
+                break;
+            default:
+                typeCompare = -1;
+                break;
+        }
     }
 
     //doesn't need to be put into update
@@ -20,10 +35,20 @@ public class Patron_Collide : MonoBehaviour
 
         //check if mans is aggro
 
-        //if(ammo name is same a wanted drink)
-            //points, and stuff
+        if(collideObject.tag == "Projectile"){
+            if(collideObject.GetComponent<ShotType>().type == typeCompare)
+            {
+                Debug.Log("Patron Satisfied");
+            }
+            else
+            {
+                Debug.Log("Oops");
+            }
+
+
+        }
         //else
-            //
+            //who knows if we need to handle other things colliding
 
     }
 
