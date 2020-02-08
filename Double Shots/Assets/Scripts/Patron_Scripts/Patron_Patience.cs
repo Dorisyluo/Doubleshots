b9ccbe324b5c9c03ecaa648ae8aaa938921b0a85
YashVSolanki <38870520+YashVSolanki@users.noreply.github.com>
2020-02-07 22:37:25 -0800

    Basic Patience UI in place

--- Double Shots/Assets/Scripts/Patron_Scripts/Patron_Patience.cs
+++ Double Shots/Assets/Scripts/Patron_Scripts/Patron_Patience.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Patron_Patience : MonoBehaviour
 {
     private float patience;
+    public GameObject patienceUI;
+    public Slider patienceSilder;
     // Start is called before the first frame update
     void Start()
     {
         patience = GetComponent<Patron_Data>().secsSeatPatience;
+        patienceSilder.maxValue = GetComponent<Patron_Data>().secsSeatPatience;
+        patienceSilder.value = GetComponent<Patron_Data>().secsSeatPatience;
+        patienceUI.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,6 +22,8 @@ public class Patron_Patience : MonoBehaviour
         if (GetComponent<Patron_Data>().atSeat && !GetComponent<Patron_Data>().isSatisfied)
         {
             patience -= Time.deltaTime;
+            patienceSilder.value = patience;
+            patienceUI.SetActive(true);
         }
         noPatience();
   
