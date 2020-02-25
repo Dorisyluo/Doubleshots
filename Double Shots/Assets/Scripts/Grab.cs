696f0030769131ed9cc96b7df1dfca8fb3bc4778
Lorenzo <lrelos@ucsc.edu>
2020-02-24 19:53:38 -0800

    Better Crank

--- Double Shots/Assets/Scripts/Grab.cs
+++ Double Shots/Assets/Scripts/Grab.cs
@@ -9,7 +9,7 @@ public class Grab : MonoBehaviour
 
         public void OnTriggerEnter(Collider other)
         {
-            if (other.gameObject.CompareTag("Ammo"))
+            if (other.gameObject.CompareTag("Ammo")|| other.gameObject.CompareTag("Handle"))
             {
                 CollidingObject = other.gameObject;
             
@@ -38,21 +38,40 @@ public class Grab : MonoBehaviour
         public void GrabObject() //create parentchild relationship between object and hand so object follows hand
 
         {
-            objectInHand = CollidingObject;
-            objectInHand.transform.SetParent(this.transform);
-            objectInHand.GetComponent<Rigidbody>().isKinematic = true;
-            objectInHand.GetComponent<Rigidbody>().useGravity = false;
-            objectInHand.GetComponent<ShotType>().grabbed = true;
-    }
+            if (CollidingObject.CompareTag("Handle"))
+            {
+                objectInHand = CollidingObject;
+                gameObject.AddComponent<FixedJoint>();
+                gameObject.GetComponent<FixedJoint>().connectedBody = CollidingObject.GetComponent<Rigidbody>();
+            }
+            else
+            {
+                objectInHand = CollidingObject;
+                objectInHand.transform.SetParent(this.transform);
+                objectInHand.GetComponent<Rigidbody>().isKinematic = true;
+                objectInHand.GetComponent<Rigidbody>().useGravity = false;
+                objectInHand.GetComponent<ShotType>().grabbed = true;
+            }
+        }
 
 
         private void ReleaseObject() //removing parentchild relationship so you drop the object
         {
-            objectInHand.GetComponent<Rigidbody>().isKinematic = false;
-            objectInHand.GetComponent<Rigidbody>().useGravity = true;
-            objectInHand.transform.SetParent(null);
-            objectInHand.GetComponent<ShotType>().grabbed = false;
-            objectInHand = null;
+            if (objectInHand.CompareTag("Handle"))
+            {
+
+                Destroy(gameObject.GetComponent<FixedJoint>());
+                objectInHand = null;
+            }
+            else
+            {
+                objectInHand.GetComponent<Rigidbody>().isKinematic = false;
+                objectInHand.GetComponent<Rigidbody>().useGravity = true;
+                objectInHand.transform.SetParent(null);
+                objectInHand.GetComponent<ShotType>().grabbed = false;
+                objectInHand = null;
+            }
+
         }
     }
 
--- Double Shots/Assets/Scripts/Vr Interactions/CrankGrabL.cs
+++ Double Shots/Assets/Scripts/Vr Interactions/CrankGrabL.cs
@@ -6,6 +6,7 @@ public class CrankGrabL : MonoBehaviour
 {
 		public GameObject CollidingObject;
         public GameObject objectInHand;
+        public GameObject parent;
 
         public void OnTriggerEnter(Collider other)
         {
@@ -38,20 +39,38 @@ public class CrankGrabL : MonoBehaviour
         public void GrabObject() //create parentchild relationship between object and hand so object follows hand
 
         {
-            objectInHand = CollidingObject;
-            objectInHand.transform.SetParent(this.transform);
-            objectInHand.GetComponent<Rigidbody>().isKinematic = true;
-            objectInHand.GetComponent<Rigidbody>().useGravity = false;
-            objectInHand.GetComponent<ShotType>().grabbed = true;
-    }
+            if (CollidingObject.CompareTag("Handle"))
+            {
+                objectInHand = CollidingObject;
+                //gameObject.AddComponent<FixedJoint>();
+                gameObject.GetComponent<FixedJoint>().connectedBody = CollidingObject.GetComponent<Rigidbody>();
+            }
+            else
+            {
+                objectInHand = CollidingObject;
+                objectInHand.transform.SetParent(this.transform);
+                objectInHand.GetComponent<Rigidbody>().isKinematic = true;
+                objectInHand.GetComponent<Rigidbody>().useGravity = false;
+                objectInHand.GetComponent<ShotType>().grabbed = true;
+            }
+        }
 
 
         private void ReleaseObject() //removing parentchild relationship so you drop the object
         {
-            objectInHand.GetComponent<Rigidbody>().isKinematic = false;
-            objectInHand.GetComponent<Rigidbody>().useGravity = true;
-            objectInHand.transform.SetParent(null);
-            objectInHand.GetComponent<ShotType>().grabbed = false;
-            objectInHand = null;
+            if (objectInHand.CompareTag("Handle"))
+            {
+                Destroy(gameObject.GetComponent<FixedJoint>());
+                objectInHand = null;
+            }
+            else
+            {
+                objectInHand.GetComponent<Rigidbody>().isKinematic = false;
+                objectInHand.GetComponent<Rigidbody>().useGravity = true;
+                objectInHand.transform.SetParent(null);
+                objectInHand.GetComponent<ShotType>().grabbed = false;
+                objectInHand = null;
+            }
+
         }
 }
