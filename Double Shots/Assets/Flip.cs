618e7bea89448b4e943f9f147ed34c9ad41e8d96
Justin Lee <justindlee@comcast.net>
2020-03-10 03:02:50 -0700

    hammers flip
    todo - flip over time
    motion flip?

--- Double Shots/Assets/Flip.cs
+++ Double Shots/Assets/Flip.cs
@@ -17,11 +17,13 @@ public class Flip : MonoBehaviour
         {
             flipped = true;
             transform.RotateAround(hammer.position, transform.forward, 40);
+            hammer.Rotate(0, 0, -30f, Space.Self);
         }
         if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp) && flipped)
         {
             flipped = false;
             transform.RotateAround(hammer.position, transform.forward, -40);
+            hammer.Rotate(0, 0, 30f, Space.Self);
         }
     }
 }
