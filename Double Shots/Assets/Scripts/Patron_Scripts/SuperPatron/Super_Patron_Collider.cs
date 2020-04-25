c3163578e28417cf94599841b841ff925d58130d
YashVSolanki <38870520+YashVSolanki@users.noreply.github.com>
2020-04-24 17:25:19 -0700

    super patron color issue SHOULD be fixed

--- Double Shots/Assets/Scripts/Patron_Scripts/SuperPatron/Super_Patron_Collider.cs
+++ Double Shots/Assets/Scripts/Patron_Scripts/SuperPatron/Super_Patron_Collider.cs
@@ -47,6 +47,7 @@ public class Super_Patron_Collider : MonoBehaviour
 
                     observer.GetComponent<Observer_Data>().score += 5;
                     GetComponent<Patron_Data>().superWanted.RemoveAt(0);
+                    GetComponent<Patron_Data>().isSatisfied = true; //FOR TESTING
                     correctCount++;
                     if(GetComponent<Patron_Data>().superWanted.Count <= 0)
                     {
@@ -90,10 +91,10 @@ public class Super_Patron_Collider : MonoBehaviour
                 typeCompare = 4;
                 break;
             case "purple":
-                typeCompare = 6;
+                typeCompare = 5;
                 break;
             case "green":
-                typeCompare = 5;
+                typeCompare = 6;
                 break;
             default:
                 typeCompare = -1;
