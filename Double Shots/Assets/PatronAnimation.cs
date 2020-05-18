47a89b8fae111001ca741fc60bba86e4e9d3344c
Giancarlo Tomasello <gtomasel@ucsc.edu>
2020-05-18 15:37:54 -0700

    Animation Framework up
    I have started on the animation logic. Currently the patron can walk and perform a simple idle animation once stopped. Transition between the two is a bit messy

---
+++ Double Shots/Assets/PatronAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatronAnimation : MonoBehaviour
+{
+    private Vector3 previousPosition;
+    public float curspeed;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 curMove = transform.position - previousPosition;
+        curspeed = curMove.magnitude / Time.deltaTime;
+        previousPosition = transform.position;
+
+        anim.SetFloat("Speed", curspeed);
+        Debug.Log("Animator Speed: " + anim.GetFloat("Speed"));
+     
+    }
+}
