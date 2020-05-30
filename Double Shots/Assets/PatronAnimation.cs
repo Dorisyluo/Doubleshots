1e70b796694b97ab0a1e6febab9e0fb5837c7eaa
Giancarlo <gtomasel@ucsc.edu>
2020-05-30 16:49:47 -0700

    Super Patron movin
    I have added the animation logic to the Super patron and updated the animation script. Now there will be a variation to the idle every so often.

--- Double Shots/Assets/PatronAnimation.cs
+++ Double Shots/Assets/PatronAnimation.cs
@@ -6,11 +6,14 @@ public class PatronAnimation : MonoBehaviour
 {
     private Vector3 previousPosition;
     public float curspeed;
+    private float idleChange;
     Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+
+        InvokeRepeating("idle", 2, 2);
     }
 
     // Update is called once per frame
@@ -23,5 +26,11 @@ public class PatronAnimation : MonoBehaviour
         anim.SetFloat("Speed", curspeed);
         Debug.Log("Animator Speed: " + anim.GetFloat("Speed"));
      
+    }
+
+    void idle()
+    {
+        idleChange = Random.Range(0.0f, 10.0f);
+        anim.SetFloat("IdleChange", idleChange);
     }
 }
